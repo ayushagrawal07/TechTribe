31f94fee944ebdc96ebe69470aa79fd98bc5a65b
AyushAgrawal <officialayush0925@gmail.com>
2024-06-24 10:21:09 +0530

    Revert "Initial commit1"
    This reverts commit f5bb3a4f06d5df3b09a87d68a50d326469aa3376.

--- backend/index.js
+++ backend/index.js
@@ -1,81 +1,25 @@
-// import express from 'express';
-// import dbconnect from './dbconnect.js';
-// import route from './routers/Routers.js';
-// import cors from "cors";
-// import { configDotenv } from 'dotenv';
-// import path from "path"
-// configDotenv();
-
-// const PORT = process.env.PORT || 5000;
-
-// const app = express();
-
-
-// app.use(express.json());
-
-// dbconnect();
-
-
-// app.use(cors());
-
-
-// app.use('/api',route)
-// app.use(express.static(path.join(__dirname, "./client/build")))
-
-// app.get("*", (req, res) => {
-//     res.sendFile(
-//         path.join(__dirname, "./client/build/index.html"),
-//         function (err) {
-//             res.status(500).send(err)
-//         }
-//     )
-// })
-
-// app.listen((PORT),(req,res)=>{
-//     console.log(`Server started at port ${PORT}`);
-    
-//  });
- 
 import express from 'express';
 import dbconnect from './dbconnect.js';
 import route from './routers/Routers.js';
-import cors from 'cors';
-import { configDotenv } from 'dotenv';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import cors from "cors";
 
-configDotenv();
-
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 const app = express();
+
 
 app.use(express.json());
 
 dbconnect();
 
+
 app.use(cors());
 
-app.use('/api', route);
 
-// Convert __dirname for ES module scope
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+app.use('/api',route)
 
-// Serve the frontend
-app.use(express.static(path.join(__dirname, './client/build')));
-
-app.get('*', (req, res) => {
-    res.sendFile(
-        path.join(__dirname, './client/build/index.html'),
-        function (err) {
-            if (err) {
-                res.status(500).send(err);
-            }
-        }
-    );
-});
-
-app.listen(PORT, () => {
+app.listen((PORT),(req,res)=>{
     console.log(`Server started at port ${PORT}`);
-});
+    
+ });
+ 
